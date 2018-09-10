0a43f2a0d014b1742aae79b7dc22c3e5ca51de33
Roman Schilter <roman.schilter10@gmail.com>
2018-09-10 19:31:30 +0200

    Finish (i guess?) 2FA stuff

--- 03_2FA_OTP_MAIL/index.php
+++ 03_2FA_OTP_MAIL/index.php
@@ -1,4 +1,13 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+
+require 'vendor/autoload.php';
+include 'settings.php';
+
+use Mailgun\Mailgun;
+
 
 $mode = ($_POST['mode'] ?? 'default') . 'View';
 $mode();
@@ -18,22 +27,44 @@ function loginView(){
     $password = $_POST['password'];
     $username = $_POST['username'];
 
+    // insert username and password validation here
+
+    $settings = getSettings();
+
+    $mailgun = Mailgun::create($settings['apiKey']);
+    $mailgun->messages()->send($settings['domain'], [
+       'from' => 'info@mail.ch',
+        'to' => $settings['mail'],
+        'subject' => 'Token for authentication',
+        'text' => getToken()
+    ]);
+
     echo "
         <h3>Logged in as $username</h3>
-        <p>Check your mail</p>
-";
-    echo '
-        <form method="POST">
-            <input name="mode" type="hidden" value="fa" /> 
-            <input name="token" type="text" />
+        <p>Check your mail and insert token into field below</p>
+        <form method=\"POST\">
+            <input name=\"mode\" type=\"hidden\" value=\"fa\" /> 
+            <input name=\"token\" type=\"text\" />
             <button>Submit</button>
         </form>
-    ';
+    ";
 }
 
 function faView(){
-    echo 'fa';
+    $clientToken = $_POST['token'];
+    $serverToken = getToken();
+
+    if($clientToken === $serverToken){
+        echo '<h3>Yay you managed to log in. As reward you get absolutely nothing. </h3>';
+    }
+    else{
+        echo '<h3>It\'s not that hard...</h3>';
+    }
 }
 
+function getToken(){
+    // pseudo token (what do you mean this is the interesting part?)
+    return '13214';
+}
 
 

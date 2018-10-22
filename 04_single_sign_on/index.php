7c9ceff137b89a6735f06b1b3ede419a8592a34d
Roman Schilter <roman.schilter10@gmail.com>
2018-10-22 14:31:16 +0200

    Finish single sign on

--- 04_single_sign_on/index.php
+++ 04_single_sign_on/index.php
@@ -13,57 +13,35 @@ $provider = new League\OAuth2\Client\Provider\Google([
     'clientId'     => $credentials['client_id'],
     'clientSecret' => $credentials['client_secret'],
     'redirectUri'  => 'http://localhost/M138/04_single_sign_on',
-    'hostedDomain' => 'localhost',
 ]);
 
 if (!empty($_GET['error'])) {
 
-    // Got an error, probably user denied access
     exit('Got error: ' . htmlspecialchars($_GET['error'], ENT_QUOTES, 'UTF-8'));
-
 } elseif (empty($_GET['code'])) {
-
-    // If we don't have an authorization code then get one
     $authUrl = $provider->getAuthorizationUrl();
     $_SESSION['oauth2state'] = $provider->getState();
     header('Location: ' . $authUrl);
+
     exit;
-
 } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-
-    // State is invalid, possible CSRF attack in progress
     unset($_SESSION['oauth2state']);
+
     exit('Invalid state');
-
 } else {
-
-    // Try to get an access token (using the authorization code grant)
     $token = $provider->getAccessToken('authorization_code', [
         'code' => $_GET['code']
     ]);
 
-    // Optional: Now you have a token you can look up a users profile data
+    // i would create a session with this token, but sadly php doesn't like sessions on localhost
+    echo 'Token: ' . $token->getToken(). '<br />';
+
     try {
-
-        // We got an access token, let's now get the owner details
-        $ownerDetails = $provider->getResourceOwner($token);
-
-        // Use these details to create a new profile
-        printf('Hello %s!', $ownerDetails->getFirstName());
-
+        $ownerDetails = $provider->getResourceOwner($token)->toArray();
+        echo 'Hallo ' . $ownerDetails['displayName'] . '<br />';
     } catch (Exception $e) {
-
-        // Failed to get user details
-        exit('Something went wrong: ' . $e->getMessage());
-
+        exit('An error occurred: ' . $e->getMessage());
     }
 
-    // Use this to interact with an API on the users behalf
-    echo $token->getToken();
-
-    // Use this to get a new access token if the old one expires
-    echo $token->getRefreshToken();
-
-    // Number of seconds until the access token will expire, and need refreshing
-    echo $token->getExpires();
+    echo 'Login erfolgreich';
 }

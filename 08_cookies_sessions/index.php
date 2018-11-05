7354a58b7692a0319fee50d1944c45fc23fb5e72
Roman Schilter <roman.schilter10@gmail.com>
2018-11-05 14:37:19 +0100

    Add csrf protection for login form (08_cookies_sessions)

--- 08_cookies_sessions/index.php
+++ 08_cookies_sessions/index.php
@@ -2,6 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
+// should be random and regenerated every time
+const CSRF_TOKEN = 'afhjfh32ur8euir8efzhcdshftbwe';
+
 $action = ($_POST['action'] ?? 'index') . 'Action';
 $action();
 
@@ -10,6 +13,7 @@ function indexAction(){
         <form method='POST'>
             <label>Save login</label>
             <input type='hidden' name='action' value='login' />
+            <input type='hidden' name='csrf' value='". CSRF_TOKEN . "' />
             <input type='checkbox' name='save' value='1'/>
             <button>Login</button>
         </form>
@@ -17,13 +21,18 @@ function indexAction(){
 }
 
 function loginAction(){
-    setcookie(
-        'loggedin',
-        'true',
-        (array_key_exists('savegit', $_POST) && $_POST['save']) ? time() + 86400 * 14 : 0
-    );
+    if(array_key_exists('csrf', $_POST) && $_POST['csrf'] === CSRF_TOKEN) {
+        setcookie(
+            'loggedin',
+            'true',
+            (array_key_exists('savegit', $_POST) && $_POST['save']) ? time() + 86400 * 14 : 0
+        );
 
-    echo 'You logged in';
+        echo 'You logged in';
+    }
+    else {
+        echo 'Consider yourself #csrf\'ed';
+    }
 }
 
 function secretAction(){
